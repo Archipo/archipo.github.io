6e1fdb292ff5e9b22ef8b205642d5d0d85be62ef
Archipo <48192523+Archipo@users.noreply.github.com>
2019-03-25 16:45:07 +0100

    Add files via upload
    Jour 3 Javascript

---
+++ main.js
@@ -0,0 +1,6 @@
+setTimeout(function() { 
+    var question = window.confirm("Hey le site xxxvidsxxx est trop bien. Veux-tu continuer sur the second best website ?");
+    if (question) {
+        window.location = 'http://www.youtube.com/watch?v=dQw4w9WgXcQ';
+    }
+}, 2000);
